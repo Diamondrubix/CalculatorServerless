3ad78889f0e0829e853a310e2d95aea33a6bf49e
Adam Arato <Diamondrubix@gmail.com>
2018-08-19 03:08:36 -0500

    fea

--- CalculatorServerless/dynamoTest.js
+++ CalculatorServerless/dynamoTest.js
@@ -2,6 +2,18 @@ let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 
+	ddb.delete({
+		TableName: 'music',
+		Key: { 'artist': 'adam', 'songTitle': 'bestSongEver' }
+	}, function (err, data) {
+		if (err) {
+			//handle error
+		} else {
+			//your logic goes here
+		}
+	});
+
+	/*
 	ddb.put({
 		TableName: 'music',
 		Item: { 'artist': 'adam', 'songTitle': 'bestSongEver' }
@@ -12,7 +24,7 @@ exports.handler = function (event, context, callback) {
 			//your logic goes here
 		}
 	});
-
+	*/
 
 	callback(null, 'Successfully executed');
 }
