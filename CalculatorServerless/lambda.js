186d4e540a457aeda0ae6cdc65202edda759fd06
Adam Arato <Diamondrubix@gmail.com>
2018-08-19 00:02:04 -0500

    cors test revert

--- CalculatorServerless/lambda.js
+++ CalculatorServerless/lambda.js
@@ -1,14 +1,6 @@
 let AWS = require('aws-sdk');
 exports.handler = function(event, context, callback) {
 
-    var response = {
-        "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Origin": "*",
-        },
-        "body": JSON.stringify(responseBody),
-        "isBase64Encoded": false
-    };
 
 	console.log("Received request with payload", event);
 	let operation = event.Operation;
