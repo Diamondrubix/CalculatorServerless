7a2e864920d39607c202b2c8acdeb6c3af4ac542
Adam Arato <Diamondrubix@gmail.com>
2018-08-19 03:47:37 -0500

    something

--- CalculatorServerless/dynamoTest.js
+++ CalculatorServerless/dynamoTest.js
@@ -3,11 +3,9 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 
 
-
-	
-	ddb.put({
+	ddb.delete({
 		TableName: 'music',
-		Item: { 'artist': 'test', 'songTitle': 'this' }
+		Key: { 'artist': 'test', 'songTitle': 'this' }
 	}, function (err, data) {
 		if (err) {
 			//handle error
@@ -15,7 +13,21 @@ exports.handler = function (event, context, callback) {
 			//your logic goes here
 		}
 	});
-	
+
+
+/*
+	ddb.put({
+		TableName: 'music',
+		Item: { 'artist': 'test1', 'songTitle': 'this' }
+	}, function (err, data) {
+		if (err) {
+			//handle error
+		} else {
+			//your logic goes here
+		}
+	});
+	*/
+
 	//callback(null, 'Successfully executed');
 
 }
