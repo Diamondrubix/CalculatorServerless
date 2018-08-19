aab412c7a2ae7d47df1cdc9d00980bd5a860dc51
Adam Arato <Diamondrubix@gmail.com>
2018-08-19 02:15:37 -0500

    corsFromBrowser

--- CalculatorServerless/lambda.js
+++ CalculatorServerless/lambda.js
@@ -1,6 +1,21 @@
 let AWS = require('aws-sdk');
 exports.handler = function(event, context, callback) {
 
+    var responseBody = {
+        "key2": "jarg",
+        "test": event.body,
+        "key1": "value1",
+        "longest": "surpise"
+    };
+
+    var response = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": JSON.stringify(responseBody),
+        "isBase64Encoded": false
+    };
 
 	console.log("Received request with payload", event);
 	let operation = event.Operation;
